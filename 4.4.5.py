1a9247bb5d67f1e3a22ea581d2d7e11e521d4c7a
Zeradeil <ivanosh0@bk.ru>
2023-01-12 10:39:10 +0700

    add code vs 1

---
+++ 4.4.5.py
@@ -0,0 +1,7 @@
+a = int(input("ввод "))
+b = int(input("ввод "))
+c = int(input("ввод "))
+if a < 45 or b < 45 or c < 45:
+    print("одно из чисел меньше 45")
+else:
+    print("все числа больше 45")
